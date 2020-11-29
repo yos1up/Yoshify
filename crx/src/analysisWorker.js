45b4c8e4c8bde262152ee4a997e63c3e28e742ae
yos1up <yos1up@gmail.com>
2020-11-29 23:50:32 +0900

    refactoring

--- crx/src/analysisWorker.js
+++ crx/src/analysisWorker.js
@@ -1,33 +1,46 @@
+let showDebugInfo = true
 function computeBPM(data){
-    const {audioContextSampleRate, wave, waveFinishedAt} = data
+    const {waveSampleRate, wave, waveFinishedAt} = data
     // 1. STFTする
     // 2. 発音タイミングを抽出する
     // 3. その時間挙動を FFT する
-    const binSample = 1024
+    const targetWindowSecondOfSTFT = 0.025
+    const binSample = findNearestPowerOf2(waveSampleRate * targetWindowSecondOfSTFT)
     const stfts = []
+    // i ステップ目で，元の wave の [i * binSample - binSample/2, i * binSample + binSample/2) サンプルを
+    // FFT にかけたい．
+    const wavePadded = Array(binSample/2).fill(0).concat(wave).concat(Array(binSample/2).fill(0))
     for(let i=0;i<wave.length;i+=binSample){
-        let wavelet = wave.slice(i, i + binSample)
-        wavelet = padZero(wavelet, binSample)
+        let wavelet = wavePadded.slice(i, i + binSample)
+        // wavelet = padZero(wavelet, binSample)
         stfts.push(getPower(fft1(wavelet)))
     }
     // 2
-    const lpFreq = 200, hpFreq = 50000 // 低音域と高音域だけあてにする
-    const filteredIndices = [...Array(binSample).keys()].filter(i => i/binSample * audioContextSampleRate < lpFreq || hpFreq < i/binSample * audioContextSampleRate)
-    const onSignal = [...Array(stfts.length - 1).keys()].map(i => filteredIndices.map(j => Math.max(stfts[i+1][j] - stfts[i][j], 0)).reduce((x,y) => x + y))
-    const onSignalSamplingRate = audioContextSampleRate / binSample
+    const lpFreq = 300, hpFreq = 50000 // 低音域と高音域だけあてにする
+    const filteredIndices = [...Array(binSample).keys()].filter(i => i/binSample * waveSampleRate < lpFreq || hpFreq < i/binSample * waveSampleRate)
+    let onSignal = [...Array(stfts.length - 1).keys()].map(i => filteredIndices.map(j => Math.max(stfts[i+1][j] - stfts[i][j], 0)).reduce((x,y) => x + y))
+    // onSignal = onSignal.map(Math.log1p) // TODO: log1pは妥当か？
+    const onSignalSamplingRate = waveSampleRate / binSample
     // 3
-    const onSignalPadded = padZero(onSignal, 32768)
+    const targetBPMResolution = 0.1 // BPM の目標解像度（indexToBPMCoeff がこれくらいの値になって欲しい） 
+    const onSignalPaddedLength = findNearestPowerOf2(60 * waveSampleRate / binSample / targetBPMResolution)
+    const onSignalPadded = padZero(onSignal, onSignalPaddedLength)
     const onSpect = fft1(onSignalPadded)
     const onPowerSpect = getPower(onSpect)
-    const indexToBPMCoeff = 60 * audioContextSampleRate / onSignalPadded.length / binSample
+    const indexToBPMCoeff = 60 * waveSampleRate / onSignalPadded.length / binSample
 
-    const peakIndex = findPeak(onPowerSpect, Math.floor(50 / indexToBPMCoeff), Math.ceil(200 / indexToBPMCoeff))
+    if (showDebugInfo){
+        console.log({binSample, onSignalPaddedLength})
+        showDebugInfo = false
+    }
+
+    const peakIndex = findPeak(onPowerSpect, Math.floor(84.5 / indexToBPMCoeff), Math.ceil(200.5 / indexToBPMCoeff))
     const bpmCandidates = peakIndex.map(i => ({
         "BPM": indexToBPMCoeff * i,
         "peakValue": onPowerSpect[i],
         "curvature": onPowerSpect[i] - 0.5 * (onPowerSpect[i-1] + onPowerSpect[i+1])
     }))
-    return {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave}
+    return {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave, waveSampleRate, stfts}
 }
 
 function findPeak(ary, start, end){
@@ -96,5 +109,9 @@ function getPower(cplxAry){
     return cplxAry.map(e => e[0]*e[0] + e[1]*e[1])
 }
 // ====================== FFT ======================
+
+function findNearestPowerOf2(x){
+    return 1 << Math.round(Math.log2(x))
+}
 
 onmessage = function(e) {postMessage(computeBPM(e.data))}
--- crx/src/main.js
+++ crx/src/main.js
@@ -133,9 +133,9 @@ if (vs.length > 0){
         track.connect(spn)
         spn.connect(audioContext.destination)
     }else{
-        const processorPath = chrome.extension.getURL("src/my-processor.js")
+        const processorPath = chrome.extension.getURL("src/recordingAudioWorklet.js")
         audioContext.audioWorklet.addModule(processorPath).then(() => {
-            const awn = new AudioWorkletNode(audioContext, "my-processor")
+            const awn = new AudioWorkletNode(audioContext, "recordingAudioWorklet")
             track.connect(awn)
             // awn は destination に繋がなくても動作してくれるようだ．素敵．
             track.connect(audioContext.destination)
@@ -162,7 +162,7 @@ function computeBPM2(wave, waveSampleRate, waveFinishedAt){
         estimationResult.bpmHistory.push(null)
         return null
     }
-    worker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
+    if (analysisWorker) analysisWorker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
 }
 
 if (!USE_AUDIOWORKLET){
@@ -176,133 +176,12 @@ if (!USE_AUDIOWORKLET){
             estimationResult.bpmHistory.push(null)
             return null
         }
-        worker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
+        if (analysisWorker) analysisWorker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
     }
 
     console.log("start computing BPM")
     setInterval(computeBPM, 4000)
 }
-
-
-const workerJS = `
-    let showDebugInfo = true
-    function computeBPM(data){
-        const {waveSampleRate, wave, waveFinishedAt} = data
-        // 1. STFTする
-        // 2. 発音タイミングを抽出する
-        // 3. その時間挙動を FFT する
-        const targetWindowSecondOfSTFT = 0.025
-        const binSample = findNearestPowerOf2(waveSampleRate * targetWindowSecondOfSTFT)
-        const stfts = []
-        // i ステップ目で，元の wave の [i * binSample - binSample/2, i * binSample + binSample/2) サンプルを
-        // FFT にかけたい．
-        const wavePadded = Array(binSample/2).fill(0).concat(wave).concat(Array(binSample/2).fill(0))
-        for(let i=0;i<wave.length;i+=binSample){
-            let wavelet = wavePadded.slice(i, i + binSample)
-            // wavelet = padZero(wavelet, binSample)
-            stfts.push(getPower(fft1(wavelet)))
-        }
-        // 2
-        const lpFreq = 300, hpFreq = 50000 // 低音域と高音域だけあてにする
-        const filteredIndices = [...Array(binSample).keys()].filter(i => i/binSample * waveSampleRate < lpFreq || hpFreq < i/binSample * waveSampleRate)
-        let onSignal = [...Array(stfts.length - 1).keys()].map(i => filteredIndices.map(j => Math.max(stfts[i+1][j] - stfts[i][j], 0)).reduce((x,y) => x + y))
-        // onSignal = onSignal.map(Math.log1p) // TODO: log1pは妥当か？
-        const onSignalSamplingRate = waveSampleRate / binSample
-        // 3
-        const targetBPMResolution = 0.1 // BPM の目標解像度（indexToBPMCoeff がこれくらいの値になって欲しい） 
-        const onSignalPaddedLength = findNearestPowerOf2(60 * waveSampleRate / binSample / targetBPMResolution)
-        const onSignalPadded = padZero(onSignal, onSignalPaddedLength)
-        const onSpect = fft1(onSignalPadded)
-        const onPowerSpect = getPower(onSpect)
-        const indexToBPMCoeff = 60 * waveSampleRate / onSignalPadded.length / binSample
-
-        if (showDebugInfo){
-            console.log({binSample, onSignalPaddedLength})
-            showDebugInfo = false
-        }
-
-        const peakIndex = findPeak(onPowerSpect, Math.floor(84.5 / indexToBPMCoeff), Math.ceil(200.5 / indexToBPMCoeff))
-        const bpmCandidates = peakIndex.map(i => ({
-            "BPM": indexToBPMCoeff * i,
-            "peakValue": onPowerSpect[i],
-            "curvature": onPowerSpect[i] - 0.5 * (onPowerSpect[i-1] + onPowerSpect[i+1])
-        }))
-        return {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave, waveSampleRate, stfts}
-    }
-
-    function findPeak(ary, start, end){
-        start = Math.max(0, start)
-        end = Math.min(ary.length, end)
-        let ret = null, maxi = -Infinity
-        for(let i=start;i<end;i++){
-            if (maxi < ary[i]) {
-                maxi = ary[i]
-                ret = i
-            }
-        }
-        let ret2 = []
-        for(let i=start;i<end;i++){
-            if (0 < i && i + 1 < ary.length && ary[i-1] <= ary[i] && ary[i] >= ary[i+1]) ret2.push(i)
-        }
-        return ret2
-    }
-
-    // ====================== FFT ======================
-    // https://qiita.com/bellbind/items/ba7aa07f6c915d400000
-    function expi(theta) {return [Math.cos(theta), Math.sin(theta)];}
-    function iadd([ax, ay], [bx, by]) {return [ax + bx, ay + by];}
-    function isub([ax, ay], [bx, by]) {return [ax - bx, ay - by];}
-    function imul([ax, ay], [bx, by]) {return [ax * bx - ay * by, ax * by + ay * bx];}
-    function isum(cs) {return cs.reduce((s, c) => iadd(s, c), [0, 0]);}
-    function revBit(k, n) {
-        let r = 0;
-        for (let i = 0; i < k; i++) r = (r << 1) | ((n >>> i) & 1);
-        return r;
-    }
-    function fftin1(c, T, N) {
-        const k = Math.log2(N);
-        const rec = c.map((_, i) => c[revBit(k, i)]);
-        for (let Nh = 1; Nh < N; Nh *= 2) {
-            T /= 2;
-            for (let s = 0; s < N; s += Nh * 2) {
-                for (let i = 0; i < Nh; i++) {
-                    const l = rec[s + i], re = imul(rec[s + i + Nh], expi(T * i));
-                    [rec[s + i], rec[s + i + Nh]] = [iadd(l, re), isub(l, re)];
-                }
-            }
-        }
-        return rec;
-    }
-    function fft1(f) {
-        /* [[re, im], ...] => [[re, im], ...] array length must be power of 2 */
-        f = f.map(e => (typeof e[0] === "undefined") ? [e, 0] : e) // 実数は複素数に
-        const N = f.length, T = -2 * Math.PI;
-        return fftin1(f, T, N);
-    }
-    function ifft1(F) {
-        /* [[re, im], ...] => [[re, im], ...] array length must be power of 2 */
-        F = F.map(e => (typeof e[0] === "undefined") ? [e, 0] : e) // 実数は複素数に
-        const N = F.length, T = 2 * Math.PI;
-        return fftin1(F, T, N).map(([r, i]) => [r / N, i / N]);
-    }
-    function padZero(ary, len){
-        if (typeof len === "undefined"){
-            len = 1
-            while(len < ary.length) len <<= 1
-        }
-        return ary.concat(Array(len - ary.length).fill(0))
-    }
-    function getPower(cplxAry){
-        return cplxAry.map(e => e[0]*e[0] + e[1]*e[1])
-    }
-    // ====================== FFT ======================
-
-    function findNearestPowerOf2(x){
-        return 1 << Math.round(Math.log2(x))
-    }
-
-    onmessage = function(e) {postMessage(computeBPM(e.data))}
-`
 
 let estimationResult = {
     "bpm": null,
@@ -310,39 +189,42 @@ let estimationResult = {
     "bpmHistory": []
 }
 
-let worker = new Worker(createURLFromText(workerJS))
-let debugKey = localStorage.save
-worker.onmessage = function (e){
-    const {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave, waveSampleRate, stfts} = e.data
-    if (bpmCandidates.length > 0) {
-        const argmaxi = argmax(bpmCandidates.map(_ => _.peakValue))
-        const bpm = bpmCandidates[argmaxi].BPM
-        console.log("estimated BPM: " + bpm)
+let analysisWorker = null
+fetch(chrome.extension.getURL("src/analysisWorker.js")).then((res) => res.text()).then((text)=>{
+    analysisWorker = new Worker(createURLFromText(text))
+    let debugKey = localStorage.save
+    analysisWorker.onmessage = function (e){
+        const {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave, waveSampleRate, stfts} = e.data
+        if (bpmCandidates.length > 0) {
+            const argmaxi = argmax(bpmCandidates.map(_ => _.peakValue))
+            const bpm = bpmCandidates[argmaxi].BPM
+            console.log("estimated BPM: " + bpm)
 
 
-        //拍タイミング推定
-        const peakOffset = findPeakOffset(onSignal, onSignalSamplingRate * 60 / bpm)
-        const waveStartedAt = waveFinishedAt - wave.length / waveSampleRate
-        // const beatReferenceTime = waveFinishedAt - (onSignal.length - peakOffset) / onSignalSamplingRate
-        const beatReferenceTime = waveStartedAt + peakOffset / onSignalSamplingRate
+            //拍タイミング推定
+            const peakOffset = findPeakOffset(onSignal, onSignalSamplingRate * 60 / bpm)
+            const waveStartedAt = waveFinishedAt - wave.length / waveSampleRate
+            // const beatReferenceTime = waveFinishedAt - (onSignal.length - peakOffset) / onSignalSamplingRate
+            const beatReferenceTime = waveStartedAt + peakOffset / onSignalSamplingRate
 
-        estimationResult.bpm = bpm
-        estimationResult.beatReferenceTime = beatReferenceTime
-        estimationResult.bpmHistory.push(bpm)
-        const num = 11
-        console.log(`(median of last ${num} trials: ${getMedian(estimationResult.bpmHistory.slice(-num))})`)
+            estimationResult.bpm = bpm
+            estimationResult.beatReferenceTime = beatReferenceTime
+            estimationResult.bpmHistory.push(bpm)
+            const num = 11
+            console.log(`(median of last ${num} trials: ${getMedian(estimationResult.bpmHistory.slice(-num))})`)
 
-        // デバッグ用
-        if (localStorage.save != debugKey){
-            debugKey = localStorage.save
-            autoDownload("snapshot.txt", JSON.stringify({
-                bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave,
-                bpm, peakOffset, beatReferenceTime, 
-                waveSampleRate, stfts
-            }))
+            // デバッグ用
+            if (localStorage.save != debugKey){
+                debugKey = localStorage.save
+                autoDownload("snapshot.txt", JSON.stringify({
+                    bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave,
+                    bpm, peakOffset, beatReferenceTime, 
+                    waveSampleRate, stfts
+                }))
+            }
         }
     }
-}
+})
 
 // メトロノーム用
 let lastScheduledTime = null
