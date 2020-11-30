7c71cf5371c6bbf223d060ad709f0b78fdce6489
yos1up <yos1up@gmail.com>
2020-12-01 00:21:26 +0900

    support nicovideo.jp

--- crx/src/main.js
+++ crx/src/main.js
@@ -6,45 +6,6 @@ const rhythmPattern = [
     {"inst": "high", "pos": 0.76},
 ]
 
-let playMetronome = false
-chrome.extension.onRequest.addListener (
-    function(request, sender, sendResponse) {
-        playMetronome = request.isActive
-        if (playMetronome) playSound(soundBuffers.yoshi)
-        sendResponse({})
-        return true
-    }
-)
-
-
-function argmax(ary){
-    let maxi = -Infinity, argmaxi = -1
-    for(let i=0;i<ary.length;i++) if (maxi < ary[i]) {maxi = ary[i]; argmaxi = i}
-    return argmaxi
-}
-
-function autoDownload(filename, contentString){
-    download(filename, createURLFromText(contentString))
-}
-function createURLFromText(contentString) {
-    let bom = new Uint8Array([0xef, 0xbb, 0xbf])
-    let url = URL.createObjectURL(
-        new Blob([bom, contentString], { type: 'text/plain;charset=UTF-8' })
-    )
-    return url
-}
-function download(filename, url){
-    let a = document.createElement('a')
-    document.body.appendChild(a)
-    a.download = filename
-    a.href = url
-    a.click()
-    document.body.removeChild(a)
-}
-
-const audioContext = new AudioContext()
-const audioContextSampleRate = audioContext.sampleRate
-
 const soundBuffers = {}
 function loadSound(url, name) {
     let request = new XMLHttpRequest();
@@ -58,21 +19,10 @@ function loadSound(url, name) {
     }
     request.send()
 }
-loadSound(chrome.extension.getURL("../sounds/bongo_low.wav"), "low")
-loadSound(chrome.extension.getURL("../sounds/bongo_high.wav"), "high")
-loadSound(chrome.extension.getURL("../sounds/yoshi.wav"), "yoshi")
-
-
 function playSound(buffer, when) {
     /* デコード済音声バイナリデータ buffer を when[sec]（AudioContextのcurrentTimeなどで扱われる絶対時間）に再生する */
     let source = audioContext.createBufferSource()
     source.buffer = buffer
-    let gainNode = audioContext.createGain()
-    /*
-    source.connect(gainNode)
-    gainNode.connect(audioContext.destination)   
-    gainNode.gain.value = 0.5
-    */
     source.connect(audioContext.destination)
     source.start(when)
 }
@@ -103,57 +53,221 @@ class RingBuffer{
         return this.data.slice(s).concat(this.data.slice(0, n - this.length + s))
     }
 }
-const waveBuffer = new RingBuffer(audioContextSampleRate * 5)
+let waveBuffer = null
 
-// ScriptProcessorNode を用いる方針（非推奨，いずれは AudioWorklet へ移行が必要らしいが）
-// TODO: 16384設定（最大）でもSPNはかなり重い
-// AudioWorklet へ移行すべきか．あるいは destination へのルートと spn へのルートを並列にできないか？
-const binWidthBySample = 16384
+let playMetronome = false
+chrome.extension.onRequest.addListener (
+    function(request, sender, sendResponse) {
+        playMetronome = request.isActive
+        if (playMetronome) playSound(soundBuffers.yoshi)
+        sendResponse({})
+        return true
+    }
+)
 
-let vs = document.getElementsByTagName("video")
-if (vs.length > 0){
-    const track = audioContext.createMediaElementSource(vs[0])
+let estimationResult = {
+    "bpm": null,
+    "beatReferenceTime": null,
+    "bpmHistory": []
+}
+
+let analysisWorker = null
+
+let audioContext = null
+let audioContextSampleRate = null
+
+// =========================================
+
+
+function argmax(ary){
+    let maxi = -Infinity, argmaxi = -1
+    for(let i=0;i<ary.length;i++) if (maxi < ary[i]) {maxi = ary[i]; argmaxi = i}
+    return argmaxi
+}
+
+function autoDownload(filename, contentString){
+    download(filename, createURLFromText(contentString))
+}
+function createURLFromText(contentString) {
+    let bom = new Uint8Array([0xef, 0xbb, 0xbf])
+    let url = URL.createObjectURL(
+        new Blob([bom, contentString], { type: 'text/plain;charset=UTF-8' })
+    )
+    return url
+}
+function download(filename, url){
+    let a = document.createElement('a')
+    document.body.appendChild(a)
+    a.download = filename
+    a.href = url
+    a.click()
+    document.body.removeChild(a)
+}
+
+function getRunningAudioContext(){
+    /*
+        audioContext.state === "running" な audioContext が
+        得られたらそれを返す
+        （ユーザがページ内で操作をすると得られるようになる）
+    */
+    return new Promise((resolve, reject) => {
+        const handler = setInterval(()=>{
+            const audioContext = new AudioContext()
+            if (audioContext.state === "running"){
+                clearInterval(handler)
+                resolve(audioContext)
+            }
+        }, 1000)
+    })
+}
+
+getRunningAudioContext().then((ac)=>{
+    audioContext = ac
+    audioContextSampleRate = audioContext.sampleRate
+    waveBuffer = new RingBuffer(audioContextSampleRate * 5)
+
+    loadSound(chrome.extension.getURL("../sounds/bongo_low.wav"), "low")
+    loadSound(chrome.extension.getURL("../sounds/bongo_high.wav"), "high")
+    loadSound(chrome.extension.getURL("../sounds/yoshi.wav"), "yoshi")
+
+    // ScriptProcessorNode を用いる方針（非推奨，いずれは AudioWorklet へ移行が必要らしいが）
+    // TODO: 16384設定（最大）でもSPNはかなり重い
+    // AudioWorklet へ移行すべき → した (USE_AUDIOWORKLET == true で)
+    const binWidthBySample = 16384
+
+    let vs = document.getElementsByTagName("video")
+    console.log(`number of video elements: ${vs.length}`)
+    if (vs.length > 0){
+        const track = audioContext.createMediaElementSource(vs[0])
+        // TODO: もしかしてタブの音声出力全掴みできる？
+        if (!USE_AUDIOWORKLET){
+            const spn = audioContext.createScriptProcessor(binWidthBySample, 1, 1)
+            // 生波形をバッファに貯めていく
+            spn.onaudioprocess = function(e) {
+                var inputBuffer = e.inputBuffer
+                var outputBuffer = e.outputBuffer
+                for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+                    var inputData = inputBuffer.getChannelData(channel);
+                    var outputData = outputBuffer.getChannelData(channel);
+                    for (var sample = 0; sample < inputBuffer.length; sample++) {
+                        outputData[sample] = inputData[sample];
+                        if (channel==0) waveBuffer.push(inputData[sample])
+                        // TODO: 片方チャンネルのみになってる
+                    }
+                }
+                waveBuffer.updatedAt = audioContext.currentTime
+            }
+            track.connect(spn)
+            spn.connect(audioContext.destination)
+        }else{
+            const processorPath = chrome.extension.getURL("src/recordingAudioWorklet.js")
+            audioContext.audioWorklet.addModule(processorPath).then(() => {
+                const awn = new AudioWorkletNode(audioContext, "recordingAudioWorklet")
+                track.connect(awn)
+                console.log("track -> awn")
+                // awn は destination に繋がなくても動作してくれるようだ．素敵．
+                track.connect(audioContext.destination)
+                console.log("track -> destination")
+
+                awn.port.onmessage = (e) => {
+                    const {wave, waveSampleRate, waveFinishedAt} = e.data
+                    // const waveBuffer = e.data
+                    // const wave = waveBuffer.slice()
+                    /*
+                    const waveFinishedAt = waveBuffer.updatedAt
+                    waveBuffer.slice = RingBuffer.prototype.slice
+                    const wave = waveBuffer.slice()
+                    */
+                    computeBPM2(wave, waveSampleRate, waveFinishedAt)
+                }
+            })
+        }
+    }
+
     if (!USE_AUDIOWORKLET){
-        const spn = audioContext.createScriptProcessor(binWidthBySample, 1, 1)
-        // 生波形をバッファに貯めていく
-        spn.onaudioprocess = function(e) {
-            var inputBuffer = e.inputBuffer
-            var outputBuffer = e.outputBuffer
-            for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-                var inputData = inputBuffer.getChannelData(channel);
-                var outputData = outputBuffer.getChannelData(channel);
-                for (var sample = 0; sample < inputBuffer.length; sample++) {
-                    outputData[sample] = inputData[sample];
-                    if (channel==0) waveBuffer.push(inputData[sample])
-                    // TODO: 片方チャンネルのみになってる
+        function computeBPM(){
+            const wave = waveBuffer.slice()
+            const waveSampleRate = audioContextSampleRate
+            const waveFinishedAt = waveBuffer.updatedAt
+            if (wave.length === 0 || wave.reduce((x,y)=>x+y) === 0){
+                estimationResult.bpm = null
+                estimationResult.beatReferenceTime = null
+                estimationResult.bpmHistory.push(null)
+                return null
+            }
+            if (analysisWorker) analysisWorker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
+        }
+
+        console.log("start computing BPM")
+        setInterval(computeBPM, 4000)
+    } 
+
+    fetch(chrome.extension.getURL("src/analysisWorker.js")).then((res) => res.text()).then((text)=>{
+        analysisWorker = new Worker(createURLFromText(text))
+        let debugKey = localStorage.save
+        analysisWorker.onmessage = function (e){
+            const {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave, waveSampleRate, stfts} = e.data
+            if (bpmCandidates.length > 0) {
+                const argmaxi = argmax(bpmCandidates.map(_ => _.peakValue))
+                const bpm = bpmCandidates[argmaxi].BPM
+                console.log("estimated BPM: " + bpm)
+
+
+                //拍タイミング推定
+                const peakOffset = findPeakOffset(onSignal, onSignalSamplingRate * 60 / bpm)
+                const waveStartedAt = waveFinishedAt - wave.length / waveSampleRate
+                // const beatReferenceTime = waveFinishedAt - (onSignal.length - peakOffset) / onSignalSamplingRate
+                const beatReferenceTime = waveStartedAt + peakOffset / onSignalSamplingRate
+
+                estimationResult.bpm = bpm
+                estimationResult.beatReferenceTime = beatReferenceTime
+                estimationResult.bpmHistory.push(bpm)
+                // const num = 11
+                // console.log(`(median of last ${num} trials: ${getMedian(estimationResult.bpmHistory.slice(-num))})`)
+
+                // デバッグ用
+                if (localStorage.save != debugKey){
+                    debugKey = localStorage.save
+                    autoDownload("snapshot.txt", JSON.stringify({
+                        bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave,
+                        bpm, peakOffset, beatReferenceTime, 
+                        waveSampleRate, stfts
+                    }))
                 }
             }
-            waveBuffer.updatedAt = audioContext.currentTime
         }
-        track.connect(spn)
-        spn.connect(audioContext.destination)
-    }else{
-        const processorPath = chrome.extension.getURL("src/recordingAudioWorklet.js")
-        audioContext.audioWorklet.addModule(processorPath).then(() => {
-            const awn = new AudioWorkletNode(audioContext, "recordingAudioWorklet")
-            track.connect(awn)
-            // awn は destination に繋がなくても動作してくれるようだ．素敵．
-            track.connect(audioContext.destination)
 
-            awn.port.onmessage = (e) => {
-                const {wave, waveSampleRate, waveFinishedAt} = e.data
-                // const waveBuffer = e.data
-                // const wave = waveBuffer.slice()
-                /*
-                const waveFinishedAt = waveBuffer.updatedAt
-                waveBuffer.slice = RingBuffer.prototype.slice
-                const wave = waveBuffer.slice()
-                */
-                computeBPM2(wave, waveSampleRate, waveFinishedAt)
+        // メトロノーム用
+        let lastScheduledTime = null
+        function setMetronomeSchedule(){
+            if (playMetronome){
+                const {bpm, beatReferenceTime} = estimationResult
+                if (bpm === null || beatReferenceTime === null){
+                    return
+                }
+                const beatIntervalTime = 60 / bpm
+                // audioContext.currentTime 以降 400ms の間で，beatReferenceTime + i * beatIntervalTime と表されるもののうち
+                // lastScheduledTime 以降であるものを登録する．
+                const currentTime = audioContext.currentTime
+                const seq = generateTrimmedArithmeticSequence(
+                    beatReferenceTime,
+                    beatIntervalTime,
+                    currentTime,
+                    currentTime + 0.4 // 0.4 秒未来までのメトロノームを登録する
+                ).filter(_ => (lastScheduledTime === null) || (lastScheduledTime + 0.2 < _))
+                for (let s of seq){
+                    for (let note of rhythmPattern){
+                        playSound(soundBuffers[note.inst], s + note.pos * beatIntervalTime)
+                    }
+                }
+                if (seq.length > 0) lastScheduledTime = Math.max(...seq)
             }
-        })
-    }
-}
+        }
+        setInterval(setMetronomeSchedule, 250)
+
+    })       
+
+})
 
 function computeBPM2(wave, waveSampleRate, waveFinishedAt){
     if (wave.length === 0 || wave.reduce((x,y)=>x+y) === 0){
@@ -164,95 +278,6 @@ function computeBPM2(wave, waveSampleRate, waveFinishedAt){
     }
     if (analysisWorker) analysisWorker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
 }
-
-if (!USE_AUDIOWORKLET){
-    function computeBPM(){
-        const wave = waveBuffer.slice()
-        const waveSampleRate = audioContextSampleRate
-        const waveFinishedAt = waveBuffer.updatedAt
-        if (wave.length === 0 || wave.reduce((x,y)=>x+y) === 0){
-            estimationResult.bpm = null
-            estimationResult.beatReferenceTime = null
-            estimationResult.bpmHistory.push(null)
-            return null
-        }
-        if (analysisWorker) analysisWorker.postMessage({wave, waveSampleRate, waveFinishedAt}) // WebWorker に移動したおかげで FFT が原因でメインスレッドの処理落ちすることはなくなった
-    }
-
-    console.log("start computing BPM")
-    setInterval(computeBPM, 4000)
-}
-
-let estimationResult = {
-    "bpm": null,
-    "beatReferenceTime": null,
-    "bpmHistory": []
-}
-
-let analysisWorker = null
-fetch(chrome.extension.getURL("src/analysisWorker.js")).then((res) => res.text()).then((text)=>{
-    analysisWorker = new Worker(createURLFromText(text))
-    let debugKey = localStorage.save
-    analysisWorker.onmessage = function (e){
-        const {bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave, waveSampleRate, stfts} = e.data
-        if (bpmCandidates.length > 0) {
-            const argmaxi = argmax(bpmCandidates.map(_ => _.peakValue))
-            const bpm = bpmCandidates[argmaxi].BPM
-            console.log("estimated BPM: " + bpm)
-
-
-            //拍タイミング推定
-            const peakOffset = findPeakOffset(onSignal, onSignalSamplingRate * 60 / bpm)
-            const waveStartedAt = waveFinishedAt - wave.length / waveSampleRate
-            // const beatReferenceTime = waveFinishedAt - (onSignal.length - peakOffset) / onSignalSamplingRate
-            const beatReferenceTime = waveStartedAt + peakOffset / onSignalSamplingRate
-
-            estimationResult.bpm = bpm
-            estimationResult.beatReferenceTime = beatReferenceTime
-            estimationResult.bpmHistory.push(bpm)
-            const num = 11
-            console.log(`(median of last ${num} trials: ${getMedian(estimationResult.bpmHistory.slice(-num))})`)
-
-            // デバッグ用
-            if (localStorage.save != debugKey){
-                debugKey = localStorage.save
-                autoDownload("snapshot.txt", JSON.stringify({
-                    bpmCandidates, onSignal, onSignalSamplingRate, waveFinishedAt, onPowerSpect, wave,
-                    bpm, peakOffset, beatReferenceTime, 
-                    waveSampleRate, stfts
-                }))
-            }
-        }
-    }
-})
-
-// メトロノーム用
-let lastScheduledTime = null
-function setMetronomeSchedule(){
-    if (playMetronome){
-        const {bpm, beatReferenceTime} = estimationResult
-        if (bpm === null || beatReferenceTime === null){
-            return
-        }
-        const beatIntervalTime = 60 / bpm
-        // audioContext.currentTime 以降 400ms の間で，beatReferenceTime + i * beatIntervalTime と表されるもののうち
-        // lastScheduledTime 以降であるものを登録する．
-        const currentTime = audioContext.currentTime
-        const seq = generateTrimmedArithmeticSequence(
-            beatReferenceTime,
-            beatIntervalTime,
-            currentTime,
-            currentTime + 0.4 // 0.4 秒未来までのメトロノームを登録する
-        ).filter(_ => (lastScheduledTime === null) || (lastScheduledTime + 0.2 < _))
-        for (let s of seq){
-            for (let note of rhythmPattern){
-                playSound(soundBuffers[note.inst], s + note.pos * beatIntervalTime)
-            }
-        }
-        if (seq.length > 0) lastScheduledTime = Math.max(...seq)
-    }
-}
-setInterval(setMetronomeSchedule, 250)
 
 function findPeakOffset(ary, interval){
     // ary[i + n * interval] (nは添字が有効な範囲で整数全体を動く) の平均が最大となるような i を求める．
@@ -313,9 +338,4 @@ function getMedian(ary){
         return (sorted[len / 2 - 1] + sorted[len / 2]) / 2
     }
 }
-
-
-
-
-
 
